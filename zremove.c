49c0a5a97f5fe7e76378134294fecc2d99cc9f19
Mason <mwitcraft11@gmail.com>
2018-11-25 14:25:36 -0600

    Added zremove.c

---
+++ zremove.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "oufs_lib.h"
+
+int main(int argc, char** argv) {
+  // Fetch the key environment vars
+  char cwd[MAX_PATH_LENGTH];
+  char disk_name[MAX_PATH_LENGTH];
+  oufs_get_environment(cwd, disk_name);
+
+  // Check arguments
+  if(argc == 2) {
+    // Open the virtual disk
+    vdisk_disk_open(disk_name);
+
+    //Gets the file for writing
+    oufs_remove(cwd, argv[1]);
+
+    // Clean up
+    vdisk_disk_close();
+
+  }else{
+    // Wrong number of parameters
+    fprintf(stderr, "Usage: ztouch <dirname>\n");
+  }
+
+}
